b28e536eb7f2f6f601870320f29bd40093b8e44d
gerrygeop <gerryja28@gmail.com>
2025-11-28 19:53:34 +0800

    refactor: remove spa()

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -57,7 +57,7 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ])
-            ->databaseNotifications()
-            ->spa();
+            ->databaseNotifications();
+            // ->spa();
     }
 }
